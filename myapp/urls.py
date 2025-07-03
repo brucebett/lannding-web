dea3a69e341f491767130a9b5d1a8c7cb4fbee05
brucebett <bett6448@gmail.com>
2025-07-03 22:23:55 +0300

    thur23 commit

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,6 @@ from myapp import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('service_details/', views.service_details, name='service_details'),
+    path('starter_page/', views.starter_page, name='starter_page'),
     
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -6,3 +6,5 @@ def index(request):
     return render(request, 'index.html')  
 def service_details(request):
     return render(request, 'service-details.html')  
+def starter_page(request):
+    return render(request, 'starter-page.html')  
